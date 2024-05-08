991ff41c57302bd59bfa794c9405cb202fa0ed6b
Pascal Wittler <git@pascalwittler.de>
2024-05-09 01:53:25 +0200

    Add initialization function to Train js module

--- public/res/js/modules/train.js
+++ public/res/js/modules/train.js
@@ -2,4 +2,10 @@ class Train {
   constructor(domElement) {
     this.domElement = domElement;
   }
+
+  static initialize() {
+    (document.querySelectorAll('.train')).forEach(trainDomElement => new Train(trainDomElement));
+  }
 }
+
+Train.initialize();
